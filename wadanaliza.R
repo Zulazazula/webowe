792435a149e749c9d555eba0e3137cd377b120c8
Zuzanna Wysocka <zuzanka.wysocka@gmail.com>
2026-01-21 17:57:46 +0100

    Load data from CSV into 'data' data frame
    Added code to load data from a CSV file into a data frame.

--- wadanaliza.R
+++ wadanaliza.R
@@ -1,3 +1,7 @@
 library (tidyverse)
 install.packages("rcompanion")
+
 library(rcompanion)
+# Wczytanie danych z pliku CSV do ramki danych 'data'
+data <- read.csv("C:/Users/zuzan/OneDrive - Akademia Górniczo-Hutnicza im. Stanisława Staszica w Krakowie/WAD/students_dropout_academic_success.csv", header = TRUE, sep = ",")
+
